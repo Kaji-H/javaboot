2003b5197e30f27a5d679877a57603c7c1beb000
Khiro610 <hiroxka@gmail.com>
2021-05-05 15:07:02 +0900

    練習

--- practice/src/com/kajiH/main.java
+++ practice/src/com/kajiH/main.java
@@ -4,7 +4,9 @@ public class main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello?");
+		System.out.println("I am hiroaki kaji");
+		System.out.println("I practice program");
+		System.out.println("I will try it");
 
 	}
 
