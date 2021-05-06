11d26e5a2cbe65c8b7ccd111f1fd85de716e1846
Khiro610 <hiroxka@gmail.com>
2021-05-06 15:17:35 +0900

    06. 一般的な Java の命名規則　完了

---
+++ practice/src/com/kajiH/practice0206/DoubtfulNamingRules.java
@@ -0,0 +1,10 @@
+package com.kajiH.practice0206;
+
+public class DoubtfulNamingRules {// 1
+	public static void main(String[] args) {
+		int value = 555;// 2
+		double myDouble = 23;// 3
+		double result = value / myDouble;// 4,5,6
+		System.out.printf("%d ÷ %d = %d%n", value, myDouble, result);// 7,8,9
+	}
+}
