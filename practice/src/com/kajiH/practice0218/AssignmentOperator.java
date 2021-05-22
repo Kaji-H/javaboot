8ecba3aea97751345e6c548744e53cd06b7eab0f
kaji-H <hiroxka@gmail.com>
2021-05-22 22:47:25 +0900

    18. Java の代入演算子　完了

---
+++ practice/src/com/kajiH/practice0218/AssignmentOperator.java
@@ -0,0 +1,17 @@
+package com.kajiH.practice0218;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int a = 4;
+        int target;
+
+        target = a;
+        System.out.println("target = a:" + target);
+
+        target += a;
+        System.out.println("target += a:" + target);
+
+        target *= a;
+        System.out.println("target *= a:" + target);
+    }
+}
