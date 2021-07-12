be81c0d0c5254209b7998ef922474cdced289b60
kaji-H <hiroxka@gmail.com>
2021-07-12 22:42:58 +0900

    49　じゃんけんゲーム　挑戦回数を追加

--- practice/src/com/kajiH/practice0249/RockPaperScissorsGame1.java
+++ practice/src/com/kajiH/practice0249/RockPaperScissorsGame1.java
@@ -3,68 +3,34 @@ package com.kajiH.practice0249;
 import java.util.Random;
 import java.util.Scanner;
 
-public class RockPaperScissorsGame {
+public class RockPaperScissorsGame1 {
     private static final String[] ROCK_PAPER_SCISSERS_NAME_ARRAY = { "グー",
             "チョキ", "パー" };
-    private static final int CHALLENGE_TIMES = 3;
-    private static final int FIRST_ROUND = 1;
-    private static final int ROCK_PAPER_SCISSERS_NUM = 3;
+    private static final int HAND_NUM = 3;
     private static final int ZERO = 0;
 
     private static final String WORD_THAT_LETS_PLAY_ROCK_PAPER_SCISSORS = "じゃんけん勝負\n";
     private static final String WORD_THAT_PLEASE_INPUT_ROCK_PAPER_SCISSORS = "グーチョキパーを数字で選択してね\n";
     private static final String WORD_THAT_CHOICE = "%d : %s\n";
-    private static final String WORD_THAT_ROUND = "\n%d回戦目\n";
     private static final String WORD_THAT_PLAYERS_CHOICE = "%s(COM)と%s(Player)で...\n";
     private static final String WORD_THAT_PLEASE_INPUT_IN_RANGE = "数字を入力してください\n";
     private static final String WORD_THAT_PLEASE_INPUT_NUM = "%d ~ %d の間で入力してください\n";
-    private static final String WORD_THAT_ONE_TWO_THREE = "あいこです\n";
-
     private static final String WORD_THAT_WIN = "あなたの勝ちです\n";
     private static final String WORD_THAT_LOSE = "あなたの負けです\n";
-    private static final String WORD_THAT_LAST_RESULT = "最終結果は...　%d勝　%d敗　です\n";
 
     private static Random RAND = new Random();
     private static Scanner STDIN = new Scanner(System.in);
+    private static final String WORD_THAT_ONE_TWO_THREE = "あいこです\n";
 
     public static void main(String[] args) {
-        int count = FIRST_ROUND;
-        int numOfTimesWon = ZERO;
         boolean isWin = false;
 
         showWordThatLetsPlayRockPaperScissors();
+        showWordThatRockPaperScissors();
 
-        while (!isOverChallengeTime(count)) {
+        isWin = playRockPaperScissorsGame();
 
-            showWordThatRound(count);
-            showWordThatRockPaperScissors();
-
-            isWin = playRockPaperScissorsGame();
-
-            showResult(isWin);
-
-            numOfTimesWon += plusOneIf(isWin);
-            count++;
-        }
-
-        showLastResult(numOfTimesWon);
-    }
-
-    private static void showLastResult(int numOfTimesWon) {
-        System.out.printf(WORD_THAT_LAST_RESULT, numOfTimesWon,
-                (CHALLENGE_TIMES - numOfTimesWon));
-    }
-
-    private static int plusOneIf(boolean isWin) {
-        return isWin ? 1 : 0;
-    }
-
-    private static boolean isOverChallengeTime(int count) {
-        return count > CHALLENGE_TIMES;
-    }
-
-    private static void showWordThatRound(int count) {
-        System.out.printf(WORD_THAT_ROUND, count);
+        result(isWin);
     }
 
     private static void showWordThatLetsPlayRockPaperScissors() {
@@ -113,12 +79,11 @@ public class RockPaperScissorsGame {
     }
 
     private static boolean isRange(int inputInt) {
-        return inputInt >= ZERO && inputInt < ROCK_PAPER_SCISSERS_NUM;
+        return inputInt >= ZERO && inputInt < HAND_NUM;
     }
 
     private static void showWordThatPleaseInputNum() {
-        System.out.printf(WORD_THAT_PLEASE_INPUT_NUM, ZERO,
-                ROCK_PAPER_SCISSERS_NUM);
+        System.out.printf(WORD_THAT_PLEASE_INPUT_NUM, ZERO, HAND_NUM);
     }
 
     private static boolean isInteger(String inputStr) {
@@ -131,7 +96,7 @@ public class RockPaperScissorsGame {
     }
 
     private static int selectEnemyChoice() {
-        return RAND.nextInt(ROCK_PAPER_SCISSERS_NUM);
+        return RAND.nextInt(HAND_NUM);
     }
 
     private static void showWordThatPlayersChoice(int myChoice,
@@ -161,14 +126,14 @@ public class RockPaperScissorsGame {
     }
 
     private static boolean isMyWin(int myChoice, int enemyChoice) {
-        return (myChoice + 1) % ROCK_PAPER_SCISSERS_NUM == enemyChoice;
+        return (myChoice + 1) % HAND_NUM == enemyChoice;
     }
 
     private static boolean isSameMove(int myChoice, int enemyChoice) {
         return myChoice == enemyChoice;
     }
 
-    private static void showResult(boolean isWin) {
+    private static void result(boolean isWin) {
         if (isWin) {
             System.out.printf(WORD_THAT_WIN);
             return;
