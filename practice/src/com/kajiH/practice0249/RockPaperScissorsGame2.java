8031d319ec8a0456cacde8527260d15aaa08027f
kaji-H <hiroxka@gmail.com>
2021-07-16 23:17:40 +0900

    49 じゃんけんゲーム　チャレンジ追加　途中 コードレビュー済み　一からやり直し

--- practice/src/com/kajiH/practice0249/RockPaperScissorsGame2.java
+++ practice/src/com/kajiH/practice0249/RockPaperScissorsGame2.java
@@ -3,12 +3,17 @@ package com.kajiH.practice0249;
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+
 public class RockPaperScissorsGame {
     private static final String[] ROCK_PAPER_SCISSERS_NAME_ARRAY = { "グー",
             "チョキ", "パー" };
+    private static final String[] DIRECTIONS_NAME_ARRAY = { "上", "下", "左",
+            "右" };
     private static final int CHALLENGE_TIMES = 3;
     private static final int FIRST_ROUND = 1;
     private static final int ROCK_PAPER_SCISSERS_NUM = 3;
+    private static final int DIRECTIONS_NUM = 4;
     private static final int ZERO = 0;
 
     private static final String WORD_THAT_LETS_PLAY_ROCK_PAPER_SCISSORS = "じゃんけん勝負\n";
@@ -16,9 +21,10 @@ public class RockPaperScissorsGame {
     private static final String WORD_THAT_CHOICE = "%d : %s\n";
     private static final String WORD_THAT_ROUND = "\n%d回戦目\n";
     private static final String WORD_THAT_PLAYERS_CHOICE = "%s(COM)と%s(Player)で...\n";
-    private static final String WORD_THAT_PLEASE_INPUT_IN_RANGE = "数字を入力してください\n";
-    private static final String WORD_THAT_PLEASE_INPUT_NUM = "%d ~ %d の間で入力してください\n";
+    private static final String WORD_THAT_PLEASE_INPUT_IN_RANGE = "%d ~ %d の間で入力してください\n";
+    private static final String WORD_THAT_PLEASE_INPUT_NUM = "数字を入力してください\n";
     private static final String WORD_THAT_ONE_TWO_THREE = "あいこです\n";
+    private static final String WORD_THAT_LOOK_THAT_WAY = "あっち向いて...";
 
     private static final String WORD_THAT_WIN = "あなたの勝ちです\n";
     private static final String WORD_THAT_LOSE = "あなたの負けです\n";
@@ -38,8 +44,10 @@ public class RockPaperScissorsGame {
 
             showWordThatRound(count);
             showWordThatRockPaperScissors();
+            showWordThatRockPaperScissorsChoice();
 
-            isWin = playRockPaperScissorsGame();
+            // isWin = playRockPaperScissorsGame()
+            isWin = playLookThatWay();
 
             showResult(isWin);
 
@@ -48,6 +56,72 @@ public class RockPaperScissorsGame {
         }
 
         showLastResult(numOfTimesWon);
+    }
+
+    private static boolean playLookThatWay() {
+
+        boolean isWinInRockPaperScissors = playRockPaperScissorsGame();
+
+        showResult(isWinInRockPaperScissors);
+        showWordThatLookThatWay();
+        showWordThatDirectionsChoice();
+
+        int mySelect = selectMyDirection();
+        int enemySelect = selectEnemyDirection();
+
+        if (isWinInRockPaperScissors) {
+            return attackResult(mySelect, enemySelect);
+        }
+
+        return defendResult(mySelect, enemySelect);
+    }
+
+    private static int selectEnemyDirection() {
+        return RAND.nextInt(DIRECTIONS_NUM);
+    }
+
+    private static int selectMyDirection() {
+        String inputStr = STDIN.nextLine();
+
+        if (!isInteger(inputStr)) {
+            showWordThatPleaseInputNum();
+            return selectMyDirection();
+        }
+
+        int inputInt = Integer.parseInt(inputStr);
+
+        if (!isRangeOf(DIRECTIONS_NUM, inputInt)) {
+            showWordThatPleaseInputInRangeOf(DIRECTIONS_NUM);
+            return selectMyDirection();
+        }
+
+        return inputInt;
+    }
+
+    private static void showWordThatDirectionsChoice() {
+        for (int i = 0; i < DIRECTIONS_NUM; i++) {
+            System.out.printf(WORD_THAT_CHOICE, i, DIRECTIONS_NAME_ARRAY[i]);
+        }
+    }
+
+    private static void showWordThatLookThatWay() {
+        System.out.printf(WORD_THAT_LOOK_THAT_WAY);
+    }
+
+    private static boolean attackResult(int mySelect, int enemySelect) {
+        if (mySelect == enemySelect) {
+            return playLookThatWay();
+        }
+
+        return true;
+    }
+
+    private static boolean defendResult(int mySelect, int enemySelect) {
+        if (mySelect == enemySelect) {
+            return playLookThatWay();
+        }
+
+        return false;
     }
 
     private static void showLastResult(int numOfTimesWon) {
@@ -74,6 +148,9 @@ public class RockPaperScissorsGame {
     private static void showWordThatRockPaperScissors() {
         System.out.printf(WORD_THAT_PLEASE_INPUT_ROCK_PAPER_SCISSORS);
 
+    }
+
+    private static void showWordThatRockPaperScissorsChoice() {
         for (int i = 0; i < 3; i++) {
             System.out.printf(WORD_THAT_CHOICE, i,
                     ROCK_PAPER_SCISSERS_NAME_ARRAY[i]);
@@ -100,25 +177,24 @@ public class RockPaperScissorsGame {
 
         int inputInt = Integer.parseInt(inputStr);
 
-        if (!isRange(inputInt)) {
-            showWordThatPleaseInputInRange();
+        if (!isRangeOf(ROCK_PAPER_SCISSERS_NUM, inputInt)) {
+            showWordThatPleaseInputInRangeOf(ROCK_PAPER_SCISSERS_NUM);
             return selectMyChoice();
         }
 
         return inputInt;
     }
 
-    private static void showWordThatPleaseInputInRange() {
-        System.out.printf(WORD_THAT_PLEASE_INPUT_IN_RANGE);
+    private static void showWordThatPleaseInputInRangeOf(int maxNum) {
+        System.out.printf(WORD_THAT_PLEASE_INPUT_IN_RANGE, ZERO, maxNum);
     }
 
-    private static boolean isRange(int inputInt) {
-        return inputInt >= ZERO && inputInt < ROCK_PAPER_SCISSERS_NUM;
+    private static boolean isRangeOf(int maxNum, int inputInt) {
+        return inputInt >= ZERO && inputInt < maxNum;
     }
 
     private static void showWordThatPleaseInputNum() {
-        System.out.printf(WORD_THAT_PLEASE_INPUT_NUM, ZERO,
-                ROCK_PAPER_SCISSERS_NUM);
+        System.out.printf(WORD_THAT_PLEASE_INPUT_NUM);
     }
 
     private static boolean isInteger(String inputStr) {
