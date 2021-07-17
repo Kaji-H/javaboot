66f21b94e25a11011da9df9c160ccb4d408baf6d
kaji-H <hiroxka@gmail.com>
2021-07-17 15:14:07 +0900

    51,53　読了　54　完了

---
+++ practice/src/com/kajiH/practice0254/Customer.java
@@ -0,0 +1,29 @@
+package com.kajiH.practice0254;
+
+public class Customer {
+
+    private String name;
+    private Register regi;
+
+    public Customer(String name, int money) {
+        this.name = name;
+        this.regi = new Register(money);
+    }
+
+    public void pay() {
+        this.regi.pay();
+    }
+
+    public void showMessageOfPaid() {
+        System.out.printf("%s さんは %d 円", this.name, this.regi.getAmount());
+
+        if (this.regi.getIsPaid()) {
+            System.out.printf("支払い済みです。\n");
+            return;
+        }
+
+        System.out.printf("請求中です。\n");
+
+    }
+
+}
