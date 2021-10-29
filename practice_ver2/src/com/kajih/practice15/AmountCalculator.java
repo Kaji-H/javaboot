c978f8251c5f020023d884de27e6a9bdedd48995
kaji-H <hiroxka@gmail.com>
2021-10-30 00:16:11 +0900

    15 BigDecimal 完了

---
+++ practice_ver2/src/com/kajih/practice15/AmountCalculator.java
@@ -0,0 +1,18 @@
+package com.kajih.practice15;
+
+import java.math.BigDecimal;
+
+public class AmountCalculator {
+    public static void main(String[] args) {
+        double shippingRate = 0.3;
+
+        // new でdoubleを入れるとおかしくなるため、valueOfで使用する
+        BigDecimal strictShippingRate = BigDecimal.valueOf(shippingRate);
+        BigDecimal noteBookPrice = new BigDecimal("1000");
+
+        BigDecimal resultAmount = noteBookPrice.multiply(strictShippingRate);
+
+        System.out.format("%.0f 円のノートの送料（%.0f%%）: %n", noteBookPrice, shippingRate * 100);
+        System.out.println(resultAmount);
+    }
+}
