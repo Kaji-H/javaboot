e8494dcf500289d6733aad467aad5763c14faa61
kaji-H <hiroxka@gmail.com>
2021-12-31 15:22:07 +0900

    08 is-A has-A 完了

---
+++ practice_ver3/src/com/kajih/practice08/Bike.java
@@ -0,0 +1,11 @@
+package com.kajih.practice08;
+
+public interface Bike {
+
+    public void setColor(String color);
+
+    public void setMaxSpeed(int maxSpeed);
+
+    public void showBikeInfo();
+
+}
