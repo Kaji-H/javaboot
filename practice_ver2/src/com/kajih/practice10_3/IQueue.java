0fd1374c7a46eed3c3a63a1e6ac4b7185244a9c2
kaji-H <hiroxka@gmail.com>
2022-01-07 22:42:39 +0900

    16 トランプゲームのクラス名を変更

--- practice_ver2/src/com/kajih/practice10_3/IQueue.java
+++
@@ -1,7 +0,0 @@
-package com.kajih.practice10_3;
-
-public interface IQueue {
-    public void push(String data);
-
-    public String pop();
-}
--- practice_ver2/src/com/kajih/practice10_3/Prioritable.java
+++
@@ -1,5 +0,0 @@
-package com.kajih.practice10_3;
-
-public interface Prioritable {
-    public void push(String data, int priority);
-}
