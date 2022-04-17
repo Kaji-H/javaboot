ed11c903f9bb1362cef9bdf86da18cd39e015269
kaji-H <hiroxka@gmail.com>
2022-04-17 18:57:59 +0900

    07 根本例外を捨てる　完了

---
+++ practice_ver4/src/com/kajih/practice07/Bird.java
@@ -0,0 +1,38 @@
+package com.kajih.practice07;
+
+public class Bird {
+    protected String className;
+    protected String name;
+
+    protected Bird(String name) {
+        this.name = name;
+        this.className = "Bird";
+    }
+
+    public boolean isEmptyName() throws AppException {
+        boolean isEmptyName = false;
+        try {
+            isEmptyName = this.name.equals("");
+        } catch (NullPointerException e) {
+            // throw new AppException("UnecpectedError: 想定外の値が設定されています.");
+            throw new AppException(e);
+        }
+        return isEmptyName;
+    }
+
+    public void fly() {
+        System.out.format(
+                "[%s] %s が飛んだ. %n",
+                this.className, this.name);
+    }
+
+    public static Pigeon convertToPigeon(Bird bird) throws AppException {
+        Pigeon pigeon;
+        try {
+            pigeon = (Pigeon) bird;
+        } catch (ClassCastException e) {
+            throw new AppException("IncorrectHandlingError: 間違った処理が行われました.");
+        }
+        return pigeon;
+    }
+}
---
+++ practice_ver4/src/com/kajih/practice07/Pigeon.java
@@ -0,0 +1,8 @@
+package com.kajih.practice07;
+
+public class Pigeon extends Bird {
+    public Pigeon(String name) {
+        super(name);
+        this.className = "Pigeon";
+    }
+}
